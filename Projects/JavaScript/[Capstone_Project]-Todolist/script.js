cee850f47b47edd1646638bc03db2541b827cc92
Ferdie Nodalo <ferdienodalo7@gmail.com>
2023-07-30 11:39:34 +0800

    Capstone project todo list is done!

--- Projects/JavaScript/[Capstone_Project]-Todolist/script.js
+++ Projects/JavaScript/[Capstone_Project]-Todolist/script.js
@@ -3,36 +3,38 @@ let unOrderedList = document.querySelector('ul#list_items');
 
 form.addEventListener('submit', function (event) {
 	event.preventDefault();
-
 	let value = event.target[0].value;
-	let newListItems = document.createElement('li');
-	newListItems.setAttribute('class', 'list_container');
+	if (value.trim().length === 0) {
+		alert('Please input something');
+	} else {
+		let newListItems = document.createElement('li');
+		newListItems.setAttribute('class', 'list_container');
 
-	let paragraph = document.createElement('p');
-	paragraph.textContent = value;
+		let paragraph = document.createElement('p');
+		paragraph.textContent = value;
 
-	let checkBox = document.createElement('input');
-	checkBox.setAttribute('type', 'checkbox');
+		let checkBox = document.createElement('input');
+		checkBox.setAttribute('type', 'checkbox');
 
-	let btn = document.createElement('button');
-	btn.setAttribute('id', 'delete');
-	btn.textContent = 'delete';
+		let btn = document.createElement('button');
+		btn.setAttribute('id', 'delete');
+		btn.textContent = 'delete';
 
-	newListItems.appendChild(paragraph);
-	newListItems.prepend(checkBox);
-	newListItems.appendChild(btn);
+		newListItems.appendChild(paragraph);
+		newListItems.prepend(checkBox);
+		newListItems.appendChild(btn);
 
-	unOrderedList.appendChild(newListItems);
+		unOrderedList.appendChild(newListItems);
 
-	deleteTask(btn, newListItems);
-	isChecked(checkBox, paragraph);
+		deleteTask(btn, newListItems);
+		isChecked(checkBox, paragraph);
 
-	form.reset();
+		form.reset();
+	}
 });
 
 const isChecked = (element, listItemLine) => {
 	element.addEventListener('change', function () {
-		console.log(element.checked);
 		if (element.checked) {
 			listItemLine.setAttribute('class', 'paragraph_list');
 		} else {
